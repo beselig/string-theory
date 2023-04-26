a4fb294e3eb16307399033ab6479539629005671
Benno Selig <selig.benno@pm.me>
2023-04-26 16:06:44 +0200

    chore: placeholder title and description for page were replaced

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Boop",
-    description: "A beep boop of boops to beep booping",
+    title: "String theory",
+    description: "A gamification approach to learning to navigate the guitar and building muscle memory",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
