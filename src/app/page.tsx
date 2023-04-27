967ce04e5bdf035d48e07b9b0fc329b02e7ee50e
Benno Selig <selig.benno@pm.me>
2023-04-27 17:43:26 +0200

    feat: home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,17 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <div className="h-32 flex flex-col justify-between text-center">
-                <Link
-                    href="/game"
-                    className="p-3 bg-teal-300 text-teal-950 hover:bg-teal-400 active:bg-teal-400 active:scale-95"
-                >
-                    start game
+                <Link href="/game">
+                    <div className="rounded-md border border-teal-950 border-opacity-50 hover:border-opacity-100 transition-all p-3 w-40 h-32 flex items-center justify-around uppercase group">
+                        <div>
+                            Fretboard
+                            <br />
+                            Mastery
+                            <div className="overflow-hidden h-0 group-hover:h-5 transition-all text-base leading-none">
+                                {">"}
+                            </div>
+                        </div>
+                    </div>
                 </Link>
             </div>
         </main>
