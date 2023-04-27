5a6992f19ef4e8e3623a21ed9018c89916693a0a
Benno Selig <18337108+beselig@users.noreply.github.com>
2023-04-27 15:38:36 +0200

    feat: simpler note values
    * display simple note values without octave notation
    * play only samples randing from lowest E (E2) up one octave
    * chore: remove complex samples, keep simple octave-2 samples
    * make layout more readable
    
    BREAKING-CHANGE:
    * rewrite game-api and types

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,42 +1,46 @@
 "use client";
 import { useState } from "react";
-import { GameApi, GuitarString, Note } from "./game-api";
 import { useKeyEventHandler } from "../../shared/useKeyEventHandler";
+import { GuitarString, Note, getRandomGuitarString, getRandomNote } from "./game-api";
 
 export const Game = () => {
     const [muted, setMuted] = useState(false);
-    const pair = useStringNotePair();
-    const { guitarString, note } = pair || {};
+    const { guitarString, note } = useStringNotePair();
 
     if (!guitarString || !note) return <>Loading ...</>;
 
     return (
-        <>
-            <h1>
-                Play {note} on {guitarString}
-            </h1>
+        <div className="flex flex-col gap-4 items-center">
+            <p>Play</p>
+            <div className="flex gap-3 items-center">
+                <div className="w-14 flex justify-end text-4xl font-bold">{note}</div>
+                <div className="w-5">
+                    <span>on</span>
+                </div>
+                <div className="w-14 flex text-4xl font-bold">{guitarString}</div>
+            </div>
+            <p></p>
             <p>Hit space to generate a new note</p>
-            {muted ? null : <audio src={`/samples/${makeFileName(note)}.mp3`} autoPlay></audio>}
+            {!muted && <audio src={`/samples/simple/${makeFileName(note)}.mp3`} autoPlay></audio>}
             <button
                 role="button"
-                className="bg-slate-200 text-slate-800 hover:bg-slate-300 active:scale-95"
+                className="p-2 bg-slate-200 text-slate-800 hover:bg-slate-300 active:scale-95"
                 onClick={() => setMuted((prev) => !prev)}
             >
                 {muted ? "enable audio" : "mute audio"}
             </button>
-        </>
+        </div>
     );
 };
 
 const makeFileName = (note: Note) => {
-    return note.replace("#", "_sharp_");
+    return note.replace("#", "_sharp");
 };
 
 const getStringNotePair = (): { guitarString: GuitarString; note: Note } => {
-    const randomGuitarString = GameApi.getRandomGuitarString();
     return {
-        guitarString: randomGuitarString,
-        note: GameApi.getRandomNoteForString(randomGuitarString),
+        guitarString: getRandomGuitarString(),
+        note: getRandomNote(),
     };
 };
 
