834c36b850dfc365ebc38f465970f3a7845b30e2
Benno Selig <18337108+beselig@users.noreply.github.com>
2023-04-27 15:54:16 +0200

    feat: support touch
    * generate new note and guitarString on touchStart
    * prevent dragging on touchMove on Game page
    * create page for Game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { Game } from "@/components/Game/Game";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <div className="h-32 flex flex-col justify-between text-center">
-                <Game />
+                <Link
+                    href="/game"
+                    className="p-3 bg-teal-300 text-teal-950 hover:bg-teal-400 active:bg-teal-400 active:scale-95"
+                >
+                    start game
+                </Link>
             </div>
         </main>
     );
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -5,55 +5,60 @@ import { GuitarString, Note, getRandomGuitarString, getRandomNote } from "./game
 
 export const Game = () => {
     const [muted, setMuted] = useState(false);
-    const { guitarString, note } = useStringNotePair();
-
-    if (!guitarString || !note) return <>Loading ...</>;
+    const { guitarString, note, next, noteSampleName } = useStringNotePair();
+    useKeyEventHandler(["Space", "Enter"], () => {
+        next();
+    });
 
     return (
-        <div className="flex flex-col gap-4 items-center">
-            <p>Play</p>
-            <div className="flex gap-3 items-center">
-                <div className="w-14 flex justify-end text-4xl font-bold">{note}</div>
-                <div className="w-5">
-                    <span>on</span>
+        <div
+            className="h-full w-full touch-none flex items-center justify-around"
+            onTouchStart={next}
+        >
+            <div className="flex flex-col gap-4 items-center">
+                <p>Play</p>
+                <div className="flex gap-3 items-center">
+                    <div className="w-14 flex justify-end text-4xl font-bold">{note}</div>
+                    <div className="w-5">
+                        <span>on</span>
+                    </div>
+                    <div className="w-14 flex text-4xl font-bold">{guitarString}</div>
                 </div>
-                <div className="w-14 flex text-4xl font-bold">{guitarString}</div>
+                <p></p>
+                <p>Hit space to generate a new note</p>
+                {!muted && <audio src={`/samples/simple/${noteSampleName}.mp3`} autoPlay></audio>}
+                <button
+                    role="button"
+                    className="p-2 bg-slate-200 text-slate-800 hover:bg-slate-300 active:scale-95"
+                    onClick={() => setMuted((prev) => !prev)}
+                >
+                    {muted ? "enable audio" : "mute audio"}
+                </button>
             </div>
-            <p></p>
-            <p>Hit space to generate a new note</p>
-            {!muted && <audio src={`/samples/simple/${makeFileName(note)}.mp3`} autoPlay></audio>}
-            <button
-                role="button"
-                className="p-2 bg-slate-200 text-slate-800 hover:bg-slate-300 active:scale-95"
-                onClick={() => setMuted((prev) => !prev)}
-            >
-                {muted ? "enable audio" : "mute audio"}
-            </button>
         </div>
     );
 };
 
-const makeFileName = (note: Note) => {
-    return note.replace("#", "_sharp");
-};
-
-const getStringNotePair = (): { guitarString: GuitarString; note: Note } => {
-    return {
-        guitarString: getRandomGuitarString(),
-        note: getRandomNote(),
-    };
-};
-
 const useStringNotePair = () => {
-    const [stringNotePair, setStringNotePair] = useState<{
-        guitarString: GuitarString;
+    const makeFileName = (note: Note) => {
+        return note.replace("#", "_sharp");
+    };
+    const getStringNotePair = (): { guitarString: GuitarString; note: Note } => {
+        return {
+            guitarString: getRandomGuitarString(),
+            note: getRandomNote(),
+        };
+    };
+
+    const [{ guitarString, note }, setStringNotePair] = useState<{
         note: Note;
+        guitarString: GuitarString;
     }>(getStringNotePair());
-    console.log("stringNotePair", stringNotePair);
 
-    useKeyEventHandler(["Space", "Enter"], () => {
-        setStringNotePair(getStringNotePair());
-    });
-
-    return stringNotePair;
+    return {
+        note,
+        guitarString,
+        noteSampleName: makeFileName(note),
+        next: () => setStringNotePair(getStringNotePair()),
+    };
 };
