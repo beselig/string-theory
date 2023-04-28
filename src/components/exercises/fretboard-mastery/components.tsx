3deeffb9ba52d062d0d7c1f941120ebc9034b299
Benno Selig <selig.benno@pm.me>
2023-04-29 01:42:47 +0200

    chore: update Rules of FretboardMasteryExercise

--- src/components/exercises/fretboard-mastery/components.tsx
+++ src/components/exercises/fretboard-mastery/components.tsx
@@ -31,10 +31,7 @@ export const Rules = () => {
     return (
         <ul className="mt-2 flex list-disc flex-col gap-2 pl-5">
             <li>Apply all the knowledge you like to find the note on the string.</li>
-            <li>
-                Do not force yourself into one way of navigation use all the tools at hand to find
-                the note in question!
-            </li>
+            <li>Do not force yourself into one way of navigation.</li>
             <li>
                 Some ways to find notes are:
                 <ul className="mt-1 list-[circle] pl-5 font-extralight">
