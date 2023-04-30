9f8927584c7d2f58cac83798494b781e33eac50c
Benno Selig <18337108+beselig@users.noreply.github.com>
2023-04-30 18:45:10 +0200

    feat(FrontendMastery, Score): show note annotation in Score (#17)
    * update boxShadows in tailwind config
    * display note in Score annotation
    * less distracting layout
    * better colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,18 @@
-import { HTMLAttributes, PropsWithChildren, forwardRef } from "react";
+import { ButtonHTMLAttributes, HTMLAttributes, PropsWithChildren, forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
-const Button = forwardRef<HTMLButtonElement, PropsWithChildren<HTMLAttributes<HTMLButtonElement>>>(
-    ({ children, onClick }, ref) => {
+type ButtonProps = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>;
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ children, onClick, className, disabled }, ref) => {
         return (
             <button
+                disabled={disabled}
                 ref={ref}
-                className="flex h-[35px] items-center justify-center gap-2 rounded-[4px] bg-white px-[15px] font-medium leading-none text-black shadow-[0_2px_10px] shadow-black hover:bg-teal-100 focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none"
+                className={twMerge(
+                    "flex h-[35px] items-center justify-center gap-2 rounded-[4px] bg-slate-100 px-[15px] font-medium leading-none text-black outline-none transition-colors hover:bg-slate-200 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-400",
+                    className
+                )}
                 role="button"
                 onClick={onClick}
             >
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import Link, { LinkProps } from "next/link";
 import { AnchorHTMLAttributes, PropsWithChildren } from "react";
 import GithubMark from "../../public/images/github-mark-white.svg";
+import { HomeIcon } from "@radix-ui/react-icons";
 
 export const Navigation = () => {
     return (
-        <nav className="h-14 w-full border-b border-teal-950 bg-teal-950 dark:bg-opacity-30">
+        <nav className="box-content min-h-[60px] w-full bg-slate-800 dark:border-b dark:border-slate-800 dark:bg-black">
             <div className="container m-auto flex h-full items-center gap-4 px-4">
-                <NavItem href="/">Home</NavItem>
+                <NavItem href="/">
+                    <HomeIcon width={25} height={25} />
+                </NavItem>
                 <div className="ml-auto">
                     <NavItem href="https://github.com/beselig/string-theory" target="_blank">
                         <Image
@@ -28,7 +31,7 @@ const NavItem = (props: PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement
     return (
         <Link
             {...props}
-            className="uppercase text-teal-300 transition-colors hover:text-teal-100 hover:underline"
+            className="text-lg font-bold uppercase text-slate-300 transition-colors hover:text-slate-50"
         />
     );
 };
