f5267ee2843e370d3c53b0b0b94535cdc35ad869
Benno Selig <selig.benno@pm.me>
2023-04-30 17:53:47 +0200

    update boxShadows in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
                 "gradient-conic":
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
+            boxShadow: {
+                sm: "0 1px 2px 0 rgb(15 23 42 / 0.1)",
+                DEFAULT: "0 1px 3px 0 rgb(15 23 42 / 0.2), 0 1px 2px -1px rgb(15 23 42 / 0.2)",
+                md: "0 4px 6px -1px rgb(15 23 42 / 0.2), 0 2px 4px -2px rgb(15 23 42 / 0.2)",
+                lg: "0 10px 15px -3px rgb(15 23 42 / 0.2), 0 4px 6px -4px rgb(15 23 42 / 0.2)",
+                xl: "0 20px 25px -5px rgb(15 23 42 / 0.2), 0 8px 10px -6px rgb(15 23 42 / 0.2)",
+                "2xl": "0 25px 50px -12px rgb(15 23 42 / 0.35)",
+                inner: "inset 0 2px 4px 0 rgb(15 23 42 / 0.1)",
+                none: "none",
+            },
         },
     },
     plugins: [],
